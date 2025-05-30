7ba64ad747d437f953f0654c0cc38f5e1443e4f2
paulo3121 <bisdusty@gmail.com>
2025-05-30 18:31:29 -0300

    .

---
+++ scripts/arch.sh
@@ -0,0 +1,99 @@
+# pc antigo
+# paulo
+#!# salvar Documents/books
+#!# salvar chaves /.ssh
+#!# salvar rec/conversas novos. excluir pouco importantes
+#!# confirmar se todos os Videos/cursos_mae estao no hd
+
+
+
+# synchro
+#!# salvar workspace/scripts
+#!# salvar workspace/synpy
+
+# aur
+# yay
+# o que é e como usar
+
+# terminal
+# st, alacritty ou urxvt?
+
+# i3
+# tem mais comandos no video do cara da suica
+sudo pacman -S xorg xorg-server xorg-utils xorg-xinit xterm i3 i3-wm i3lock i3status i3blocks dmenu network-manager-applet
+sudo cp /etc/X11/xinit/xinitrc /.xinitrc
+#print screen i3
+
+
+# git
+sudo pacman -S git
+
+
+# python
+# virtualenv?
+sudo pacman -S python-virtualenv
+
+# ffmpeg
+sudo pacman -S ffmpeg
+# testar scripts rec
+
+# dotfiles
+
+# mpd
+
+
+# Synchro
+# vpn
+sudo pacman -S networkmanager-l2tp strongswan
+
+ssh
+
+# firefox
+sudo pacman -S firefox
+
+	# tdf
+        # intellij
+        # java 1.8
+        # gradle
+
+sudo pacman -S intellij-idea-community-edition jdk8-openjdk gradle
+# pacman intellij nao cria .desktop (logo, nao aparece para o dmenu. gpt sugeriu instalar pelo yay do aur). 
+# yay eh melhor?
+# como usar o gradle para gerenciar versoes java (sem o jdk8-openjdk instalado manualmente)
+
+	clojure
+	atc
+	eclipse
+	dbeaver
+	
+	front
+	# usar intellij, emacs e vscode durante migracao p nvim
+
+	ae
+	scripts
+	oci
+
+	sr8
+	pem
+
+
+Paulo
+	chrome - ublock
+	edge
+	spotify
+	soulseek - nicotine+
+	zathura
+	yt-dlp
+	virtualenv??
+	emacs
+	dotfiles
+	terminal (alacrity, st, ...)
+	bash fish zsh
+	atuin?
+	foliate
+	asdf?
+
+rec
+usp
+hub - fazer pushs
+books
