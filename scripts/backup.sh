9e4df645d228455b55abf616c45a379dc5ff8137
paulo <bisdusty@gmail.com>
2025-06-04 17:42:59 -0300

    .

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,12 +5,25 @@ BKP="$HOME/bkp/"
 
 #mkdir bkp
 
+
+# Fazer bkp
 rsync -avz --delete --progress ~/.ssh $BKP
 rsync -avz --delete --progress ~/media $BKP
 
 rsync -avz --delete --progress ~/Documents $BKP
 rsync -avz --delete --progress ~/Downloads $BKP
 
-rsync -avz --delete --progress --exclude 'eln-cache/' --exclude 'elpa/'  ~/dotfiles $BKP
 rsync -avz --delete --progress --exclude 'venv/' ~/hub $BKP
 rsync -avz --delete --progress --exclude 'venv/' ~/workspace $BKP
+rsync -avz --delete --progress --exclude 'eln-cache/' --exclude 'elpa/'  ~/dotfiles $BKP
+
+# Restaurar do bkp
+rsync -avz --delete --progress $BKP ~/.ssh
+rsync -avz --delete --progress $BKP ~/media
+
+rsync -avz --delete --progress $BKP ~/Documents
+rsync -avz --delete --progress $BKP ~/Downloads
+
+rsync -avz --delete --progress --exclude 'venv/' $BKP ~/hub
+rsync -avz --delete --progress --exclude 'venv/' $BKP ~/workspace
+rsync -avz --delete --progress --exclude 'eln-cache/' --exclude 'elpa/' $BKP ~/dotfiles
